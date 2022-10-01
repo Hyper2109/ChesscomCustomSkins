30d8c8d7eaeaa7c0eb2eb7ffe64b627d3bdd02ad
Hyper2109 <darkscorpion40@gmail.com>
2022-10-01 13:49:13 +0200

    Update skins_list.php

--- HYPER_CUSTOM_FILES/skins_list.php
+++ HYPER_CUSTOM_FILES/skins_list.php
@@ -1,1 +1,44 @@
-[{"id":"1","name":"Milk-Coffee","preview_url":"https:\/\/softool.altervista.org\/ChessSkin\/Milk-Coffee\/preview.png","style_url":"https:\/\/softool.altervista.org\/ChessSkin\/Milk-Coffee\/style.css"},{"id":"2","name":"Default All White","preview_url":"http:\/\/softool.altervista.org\/ChessSkin\/Default All White\/preview.png","style_url":"https:\/\/softool.altervista.org\/ChessSkin\/Default All White\/main.css"},{"id":"3","name":"Twitch Emotes","preview_url":"https:\/\/softool.altervista.org\/ChessSkin\/Twitch_Emotes\/preview.png","style_url":"https:\/\/softool.altervista.org\/ChessSkin\/Twitch_Emotes\/style.css"},{"id":"4","name":"Gray Scale","preview_url":"https:\/\/softool.altervista.org\/ChessSkin\/Gray_Scale\/preview.png","style_url":"https:\/\/softool.altervista.org\/ChessSkin\/Gray_Scale\/style.css"},{"id":"5","name":"Celebrity Bot","preview_url":"https:\/\/softool.altervista.org\/ChessSkin\/Celebrity_Bot\/preview.png","style_url":"https:\/\/softool.altervista.org\/ChessSkin\/Celebrity_Bot\/style.css"},{"id":"6","name":"Soft Blue","preview_url":"https://raw.githubusercontent.com/Hyper2109/test/main/soft_blue/preview.png","style_url":"https://drive.google.com/uc?export=view&id=14usbU8hRpf9AtPaFLBNfjc1bgnMUzZHz"},{"id":"7","name":"Horsey","preview_url":"HYPER_CUSTOM_FILES/custom_skins/horsey/preview.png","style_url":"https://drive.google.com/uc?export=view&id=16IO3RMm_joLsxlvwJxYZ7dzMl0CyDVm_"}]
+[
+{
+"id":"1",
+"name":"Milk-Coffee",
+"preview_url":"https:\/\/softool.altervista.org\/ChessSkin\/Milk-Coffee\/preview.png",
+"style_url":"https:\/\/softool.altervista.org\/ChessSkin\/Milk-Coffee\/style.css"
+},
+{
+"id":"2",
+"name":"Default All White",
+"preview_url":"http:\/\/softool.altervista.org\/ChessSkin\/Default All White\/preview.png",
+"style_url":"https:\/\/softool.altervista.org\/ChessSkin\/Default All White\/main.css"
+},
+{
+"id":"3",
+"name":"Twitch Emotes",
+"preview_url":"https:\/\/softool.altervista.org\/ChessSkin\/Twitch_Emotes\/preview.png",
+"style_url":"https:\/\/softool.altervista.org\/ChessSkin\/Twitch_Emotes\/style.css"
+},
+{
+"id":"4",
+"name":"Gray Scale",
+"preview_url":"https:\/\/softool.altervista.org\/ChessSkin\/Gray_Scale\/preview.png",
+"style_url":"https:\/\/softool.altervista.org\/ChessSkin\/Gray_Scale\/style.css"
+},
+{
+"id":"5",
+"name":"Celebrity Bot",
+"preview_url":"https:\/\/softool.altervista.org\/ChessSkin\/Celebrity_Bot\/preview.png",
+"style_url":"https:\/\/softool.altervista.org\/ChessSkin\/Celebrity_Bot\/style.css"
+},
+{
+"id":"6",
+"name":"Soft Blue",
+"preview_url":"https://raw.githubusercontent.com/Hyper2109/ChesscomCustomSkins/main/HYPER_CUSTOM_FILES/custom_skins/soft_blue/preview.png",
+"style_url":"https://drive.google.com/uc?export=view&id=14usbU8hRpf9AtPaFLBNfjc1bgnMUzZHz"
+},
+{
+"id":"7",
+"name":"Horsey",
+"preview_url":"https://raw.githubusercontent.com/Hyper2109/ChesscomCustomSkins/main/HYPER_CUSTOM_FILES/custom_skins/horsey/preview.png",
+"style_url":"https://drive.google.com/uc?export=view&id=16IO3RMm_joLsxlvwJxYZ7dzMl0CyDVm_"
+}
+]
